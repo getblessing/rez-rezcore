bc989329f54fdf89a6f8c82e981c358989de46d1
David Lai <davidlatwe@gmail.com>
2020-09-23 21:24:45 +0800

    Walk dir search if `system.rez_bin_path` is None

--- package.py
+++ package.py
@@ -15,6 +15,32 @@ def commands():
 
     location = system.rez_bin_path
 
+    if location is None and "REZ_USED" in os.environ:
+        # Search in resolved context
+        if system.platform == "windows":
+            bin_dir = "Scripts"
+            feature = {"activate", "rez", "pip.exe", "python.exe"}
+        else:
+            bin_dir = "bin"
+            feature = {"activate", "rez", "pip", "python"}
+
+        def up(path):
+            return os.path.dirname(path)
+
+        current = os.path.dirname(os.environ["REZ_USED"])
+        while True:
+            if bin_dir not in os.listdir(current):
+                parent_dir = up(current)
+                if current == parent_dir:
+                    break  # reached to root
+                current = parent_dir
+            else:
+                found = os.path.join(current, bin_dir)
+                content = set(os.listdir(found))
+                if content.issuperset(feature):
+                    location = os.path.join(found, "rez")
+                    break
+
     if location is None:
         raise Exception("Rez bin dir not found.")
 
